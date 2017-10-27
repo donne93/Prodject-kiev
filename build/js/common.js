e2650819692547601a0dbdda71290097c00a35d4
donne93 <donne93@mail.ru>
2017-10-27 14:31:17 +0300

    Add files via upload

--- build/js/common.js
+++ build/js/common.js
@@ -1,8 +1,22 @@
 'use strict'
 
-$(document).ready(function(){
-	$('.nav-icon').click(function(){
+$(document).ready(function() {
+    
+    $('.nav-icon').click(function(){
 		$(this).toggleClass('open');
 		$('.m-nav').slideToggle('fast');
 	});
+
+	$('.show-sort').click(function() {
+        $('.hide-select').toggleClass('active');
+    });
+    
+    $('.read-more').click(function() {
+        $('.block-with-full-text').toggleClass('active');
+    });
+    
+    $('.close-full-text').click(function() {
+        $('.block-with-full-text').toggleClass('active');
+    });
+
 });
